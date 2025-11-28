c4f7afe85e3b82ecbe5b2b8662e2b7839a912366
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-11-28 17:12:34 +0700

    first commit

---
+++ coffee-management/src/main/java/com/lebuitienthuan/coffee_management/CoffeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.lebuitienthuan.coffee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeManagementApplication.class, args);
+	}
+
+}
---
+++ coffee-management/src/test/java/com/lebuitienthuan/coffee_management/CoffeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lebuitienthuan.coffee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
